544495acab88046576a2f20637f72c004d77a2b9
Alan Ansell <jams.ansell@gmail.com>
2023-11-19 13:44:16 +0000

    try loading the columns of A into registers; a bit slower at this point

--- src/peft/tuners/sft/linear-sd/linear_sd_cuda.cu
+++ src/peft/tuners/sft/linear-sd/linear_sd_cuda.cu
@@ -64,14 +64,14 @@ std::tuple<torch::Tensor, torch::Tensor> element_ranges(
 }
 
 const int64_t PATCH_WA = 512;
-const int64_t PATCH_WB = 128;
-const int64_t PATCH_H = 16;
+const int64_t PATCH_WB = 512;
+const int64_t PATCH_H = 24;
 
 template <typename scalar_t>
 __global__ void linear_sd_backward_kernel(
     const scalar_t* __restrict__ A,
     const scalar_t* __restrict__ B,
-    const int64_t* __restrict__ Ai,
+    //const int64_t* __restrict__ Ai,
     const int64_t* __restrict__ Bi,
     const int64_t* __restrict__ pair_begins,
     const int64_t* __restrict__ pair_ends,
@@ -80,22 +80,23 @@ __global__ void linear_sd_backward_kernel(
     const int64_t h,
     float* __restrict__ outputs
 ) {
-    __shared__ float cached_A[PATCH_H * PATCH_WA];
     __shared__ float cached_B[PATCH_H * PATCH_WB];
 
-    const int64_t Ax = threadIdx.x + PATCH_WA * blockIdx.x;
+    //assert(blockDim.x == PATCH_WA);
+    const int64_t Ax = threadIdx.x + blockIdx.x * blockDim.x;
     const int64_t ybegin = blockIdx.y * PATCH_H;
-    const int64_t yend = std::min(ybegin + PATCH_H, h);
-    const int64_t yrange = yend - ybegin;
+    const int64_t yrange = min(PATCH_H, h - ybegin);
+    //assert(yrange == PATCH_H);
 
+    float cached_Acol[PATCH_H];
     if (Ax < Ad) {
-        for (int64_t i = 0; i < yrange; i++)
-            cached_A[threadIdx.x + i * PATCH_WA] = A[Ax + (i + ybegin) * Ad];
+        for (int64_t i = 0; i < PATCH_H; i++)
+            cached_Acol[i] = (i < yrange) * A[Ax + (i < yrange) * (i + ybegin) * Ad];
     }
 
     for (int64_t Bx = 0; Bx < Bd; Bx += PATCH_WB) {
         __syncthreads();
-        for (int64_t i = threadIdx.x; i < PATCH_H * PATCH_WB; i += PATCH_WA) {
+        for (int64_t i = threadIdx.x; i < PATCH_H * PATCH_WB; i += blockDim.x) {
             const int64_t Bcol = Bx + i % PATCH_WB;
             const int64_t Brow = ybegin + i / PATCH_WB;
             if (Bcol < Bd && Brow < h)
@@ -103,23 +104,20 @@ __global__ void linear_sd_backward_kernel(
         }
         __syncthreads();
 
-        const int64_t pair_id = blockIdx.x + gridDim.x * (Bx / PATCH_WB);
-        //assert(pair_id < N_pairs);
-        const int64_t pair_begin = pair_begins[pair_id];
-        const int64_t pair_end = pair_ends[pair_id];
-        for (int64_t k = pair_begin + threadIdx.x; k < pair_end; k += PATCH_WA) {
-            //assert(Ai[k] / PATCH_WA == blockIdx.x);
-            //assert(k < N_indices);
-            const int64_t Api = Ai[k] - PATCH_WA * blockIdx.x;
-            //assert(Api >= 0);
-            //assert(Api < PATCH_WA);
-            const int64_t Bpi = Bi[k] - Bx;
-            //assert(Bpi >= 0);
-            //assert(Bpi < PATCH_WB);
-            float sum = 0.0;
-            for (int64_t i = 0; i < yrange; i++)
-                sum += float(cached_A[Api + i * PATCH_WA]) * float(cached_B[Bpi + i * PATCH_WB]);
-            atomicAdd(&outputs[k], sum);
+        if (Ax < Ad) {
+            const int64_t pair_id = Ax + Ad * (Bx / PATCH_WB);
+            //assert(pair_id < N_pairs);
+            const int64_t pair_begin = pair_begins[pair_id];
+            const int64_t pair_end = pair_ends[pair_id];
+            for (int64_t k = pair_begin; k < pair_end; k++) {
+                const int64_t Bpi = Bi[k] - Bx;
+                //assert(Bpi >= 0);
+                //assert(Bpi < PATCH_WB);
+                float sum = 0.0;
+                for (int64_t i = 0; i < PATCH_H; i++)
+                    sum += float(cached_Acol[i]) * float(cached_B[Bpi + i * PATCH_WB]);
+                atomicAdd(&outputs[k], sum);
+            }
         }
     }
 }
@@ -152,11 +150,11 @@ torch::Tensor linear_sd_cuda_backward(
     //assert(torch::all(Ai < Ad).item<bool>());
     torch::Tensor Bi = di.floor_divide(Ad);
     //assert(torch::all(Bi < Bd).item<bool>());
-    torch::Tensor Apx = Ai.floor_divide(PATCH_WA);
+    //torch::Tensor Apx = Ai.floor_divide(PATCH_WA);
     //assert(torch::all(Apx < An).item<bool>());
     torch::Tensor Bpx = Bi.floor_divide(PATCH_WB);
     //assert(torch::all(Bpx < Bn).item<bool>());
-    torch::Tensor pair_ids = Apx + An * Bpx;
+    torch::Tensor pair_ids = Ai + Ad * Bpx;
     torch::Tensor pair_perm = torch::argsort(pair_ids, true);
     torch::Tensor inverse_perm = torch::empty_like(pair_perm);
     inverse_perm.index_put_(
@@ -174,11 +172,10 @@ torch::Tensor linear_sd_cuda_backward(
     //    pair_ids.index({torch::indexing::Slice(0, -1)}) <=
     //    pair_ids.index({torch::indexing::Slice(1, torch::indexing::None)})
     //).item<bool>());
-    Ai = Ai.index({pair_perm});
+    //Ai = Ai.index({pair_perm});
     Bi = Bi.index({pair_perm});
     
-    const int64_t n_pairs = An * Bn;
-    auto pair_ranges = element_ranges(pair_ids, n_pairs);
+    auto pair_ranges = element_ranges(pair_ids, Ad * Bn);
 
     torch::Tensor result = torch::zeros(
         N, 
@@ -194,7 +191,7 @@ torch::Tensor linear_sd_cuda_backward(
             linear_sd_backward_kernel<scalar_t><<<grid, PATCH_WA>>>(
                 input.data<scalar_t>(),
                 output_grad.data<scalar_t>(),
-                Ai.data<int64_t>(),
+                //Ai.data<int64_t>(),
                 Bi.data<int64_t>(),
                 std::get<0>(pair_ranges).data<int64_t>(),
                 std::get<1>(pair_ranges).data<int64_t>(),
